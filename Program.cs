aa40d45c25f540153d0a52bdbed23a052c46cad4
WhiteBob@light.com <taleshatrycob@gmail.com>
2023-11-12 21:19:18 +0500

    add WhiteGreeting

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 {
     internal class Program
     {
+        public static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
